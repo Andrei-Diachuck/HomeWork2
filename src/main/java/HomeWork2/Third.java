64aa118220502fdf5d3efa546451ef4d4a00a6e0
User <a>
2022-04-15 18:26:31 +0300

    Third

---
+++ src/main/java/HomeWork2/Third.java
@@ -0,0 +1,12 @@
+package HomeWork2;
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println(wholeNumber(-1));
+    }
+    public static boolean wholeNumber(int a){
+        while (a < 0){
+            return true;
+        }return false;
+    }
+}
